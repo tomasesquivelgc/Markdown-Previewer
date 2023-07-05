43b2178b2e112c217d588372c820f5f0640f4740
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-07-05 17:16:08 -0300

    Add font awesome icons and change project name

--- vite-project/src/components/topBar/TopBar.jsx
+++ vite-project/src/components/topBar/TopBar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsAlt, faFire } from '@fortawesome/free-solid-svg-icons';
 
-const TopBar = ({divName}) => {
-  return(
+const TopBar = ({ divName }) => {
+  return (
     <div className="toolbar">
-      <i className="fa fa-free-code-camp" title="no-stack-dub-sack"></i>
+      <i className='fa fa-free-code-camp'><FontAwesomeIcon icon={faFire} title="no-stack-dub-sack" /></i>
       {divName}
-      <i className="fa fa-arrows-alt"></i>
+      <i className='fa fa-arrows-alt'><FontAwesomeIcon icon={faArrowsAlt} /></i>
     </div>
-  )
-}
+  );
+};
 
 export default TopBar;
